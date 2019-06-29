a6a3339041721601be90a6202a52b1adbaf96841
straengel <straengel@users.noreply.github.com>
2019-06-29 22:45:40 +0300

    Добавлены стили, полезная инфа от первого задания, все приведено впорядок

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
   let money, income, addExpenses, deposit, mission, period;
   alert('Hello World Alert!');
   console.log('Hello World console.log!');
+/*
+function pow(x, n) {
+  let result = 1;
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+  return result;
+}
+let x = promt('x?', '');
+let n = prompt('n?', '');
+if (n < 0) {
+  alert('Степень ' + n +
+  'не поддерживается, введите целую степень больше 0');
+} else {
+  alert( pow(x, n) );
+}
+*/
