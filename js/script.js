050268452b62b459109b691d22de3421a53edd6d
straengel <straengel@users.noreply.github.com>
2019-06-29 20:05:44 +0300

    Вроде выполненое задание, исправленое

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
-  let money, income, addExpenses, deposit, mission, period;
-  alert('Hello World Alert!');
-  console.log('Hello World console.log!');
+let money = 12000,
+  income = 'Фрилансер',
+  addExpenses = 'Квартира, Семья, Я',
+  addExpensesArr = addExpenses.split(', '),
+  deposit = true,
+  mission = 2500000,
+  period  = 7,
+  budgetDay = money/30;
+
+console.log(money, income, deposit);
+console.log(income.length);
+console.log('Период ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+console.log(addExpenses.toLowerCase());
+console.log(addExpensesArr);
+
+for(let i = 0; i < Number(addExpensesArr.length); i++) {
+  console.log( addExpensesArr[i] );
+}
+
+console.log('дневной бюджет ' + budgetDay);
+  /*
+  function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+    result *= x;
+  }
+
+  return result;
+}
+
+let x = promt('x?', '');
+let n = prompt('n?', '');
+
+if (n < 0) {
+  alert('Степень ' + n +
+  'не поддерживается, введите целую степень больше 0');
+} else {
+  alert( pow(x, n) );
+}
+  */
