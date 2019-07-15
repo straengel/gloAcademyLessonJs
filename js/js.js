879f761d83c79a6edd186cc5b91a491fa6959aad
Sergey <straengel@gmail.com>
2019-07-15 19:22:14 +0300

    Добавлен недостающий нолик

--- js/js.js
+++ js/js.js
@@ -7,5 +7,5 @@ function addZero(str){
 }
 //Выведите на страницу текущую дату и время в формате '09:59:59 30.05.2018'
 let gDate = new Date();
-let formateDate = `${addZero(gDate.getHours())}:${addZero(gDate.getMinutes())}:${gDate.getSeconds()} ${addZero(gDate.getDate())}.${addZero(gDate.getMonth()+1)}.${addZero(gDate.getFullYear())}`;
+let formateDate = `${addZero(gDate.getHours())}:${addZero(gDate.getMinutes())}:${addZero(gDate.getSeconds())} ${addZero(gDate.getDate())}.${addZero(gDate.getMonth()+1)}.${addZero(gDate.getFullYear())}`;
 document.querySelector('#dateFirst').textContent = formateDate;
