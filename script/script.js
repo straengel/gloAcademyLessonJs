df2247cae557f0027f7091db45b6a8b0fdfae4c4
straengel <straengel@users.noreply.github.com>
2019-07-24 22:42:57 +0300

    Подготовка

--- script/script.js
+++ script/script.js
@@ -1,1 +1,55 @@
-'use strict';
+
+window.addEventListener('DOMContentLoaded', function(){
+    'use strict';
+    
+
+
+    function countTimer(deadline){
+        const   timeHours = document.querySelector('#timer-hours'),
+                timeMinutes = document.querySelector('#timer-minutes'),
+                timeSeconds = document.querySelector('#timer-seconds');
+        timeHours.textContent = '00';
+        timeMinutes.textContent = '00';
+        timeSeconds.textContent = '00';
+        function getTimeRemaining(){
+            let dateStop = new Date(deadline).getTime(),
+            dateNow = new Date().getTime(),
+            timeRemaining = (dateStop - dateNow) / 1000,
+            day = Math.floor(timeRemaining / 360 / 24),
+            hours = Math.floor((timeRemaining / 360) % 24),
+            minutes = Math.floor((timeRemaining / 60) % 60),
+            seconds = Math.floor(timeRemaining % 60);
+
+            return { hours, minutes, seconds, timeRemaining };
+        }
+        
+        function addZero(str){
+            str = String(str);
+            if(str.length < 2)
+                str = '0'+ str;
+            return str;
+        }
+
+        let timeRemaining = getTimeRemaining().timeRemaining;
+
+        function updateClock(){
+            let timer = getTimeRemaining();
+            timeHours.textContent = addZero(timer.hours);
+            timeMinutes.textContent = addZero(timer.minutes);
+            timeSeconds.textContent = addZero(timer.seconds);
+        }
+
+        
+
+
+        if(timeRemaining > 0){
+            let setInt = setInterval(updateClock, 1000);
+            setTimeout(function(){
+                clearInterval(setInt);
+            }, timeRemaining);
+        } 
+       
+    }
+    countTimer('25 july 2019');
+    //setInterval(countTimer, 1000, '29 july 2019');
+});
