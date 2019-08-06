b98a81078824ea80bbde9cafce705738fda521ff
straengel <straengel@gmail.com>
2019-08-07 01:06:08 +0300

    Готовое сложное 13 задание

--- script/script.js
+++ script/script.js
@@ -2,6 +2,41 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
     
+    // собираем все якоря; устанавливаем время анимации и количество кадров
+    const animationTime = 300,
+    framesCount = 20;
+    let anchors = [].slice.call(document.querySelectorAll('menu ul a[href*="#"], body > main > a'));
+    //document.querySelector('body > main > a');
+    console.log(anchors);
+    anchors.forEach( (item) => {
+        // каждому якорю присваиваем обработчик события
+        item.addEventListener('click', (event) => {
+            // убираем стандартное поведение
+            event.preventDefault();
+
+            // для каждого якоря берем соответствующий ему элемент и определяем его координату Y
+            let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top;
+
+            // запускаем интервал, в котором
+            let scroller = setInterval(() => {
+                // считаем на сколько скроллить за 1 такт
+                let scrollBy = coordY / framesCount;
+
+                // если к-во пикселей для скролла за 1 такт больше расстояния до элемента
+                // и дно страницы не достигнуто
+                if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+                    // то скроллим на к-во пикселей, которое соответствует одному такту
+                    window.scrollBy(0, scrollBy);
+                } else {
+                    // иначе добираемся до элемента и выходим из интервала
+                    window.scrollTo(0, coordY);
+                    clearInterval(scroller);
+                }
+                // время интервала равняется частному от времени анимации и к-ва кадров
+            }, animationTime / framesCount);
+        });
+    });
+
 
     //таймер
     function countTimer(deadline){
